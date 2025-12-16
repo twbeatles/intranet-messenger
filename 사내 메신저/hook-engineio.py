a12c0d4c434886fd840afa919afa08e8191563b0
twbeatles <soulburn7@gmail.com>
2025-12-16 15:28:26 +0900

    Delete 사내 메신저 directory

--- 사내 메신저/hook-engineio.py
+++
@@ -1,8 +0,0 @@
-# PyInstaller hook for engineio and flask-socketio
-# This file tells PyInstaller which hidden modules to include
-
-from PyInstaller.utils.hooks import collect_submodules
-
-hiddenimports = collect_submodules('engineio.async_drivers')
-hiddenimports += collect_submodules('socketio')
-hiddenimports += ['simple_websocket']
